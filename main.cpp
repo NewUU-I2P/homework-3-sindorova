8623a8f28aa3697532e553fdb036dbff091df791
sindorova <147584619+sindorova@users.noreply.github.com>
2023-11-14 17:57:34 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
     cout << "Problem 1\n";
-    problemSolution1(55);
+    problemSolution1();
     cout << "Problem 2\n";
     problemSolution2();
     cout << "Problem 3\n";
