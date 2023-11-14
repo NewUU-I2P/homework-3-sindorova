55a5c022f9aeece3a332b0d05b182ada0fb8782b
sindorova <147584619+sindorova@users.noreply.github.com>
2023-11-14 19:26:37 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,35 +1,36 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
+cout << "Enter water consumption in cubic meters: ";
+  cin >> consumption;
 
-    cost = 13.0;
-
-  // Calculate charges for first 30 cubic meters
-  if (consumed_water > 0) {
-    if (consumed_water <= 30) {
-      cost += consumed_water * 0.4;
+  // Calculate the total cost
+  cost = 13.0;
+  if (consumption > 0) {
+    if (consumption <= 30) {
+      cost += consumption * 0.4;
     } else {
-          cost += 30 * 0.4;
+      cost += 30 * 0.4;
 
-      // Calculate charges for next 20 cubic meters
-      if (consumed_water <= 50) {
-           cost += (consumed_water - 30) * 0.12;
+      if (consumption <= 50) {
+        cost += (consumption - 30) * 0.12;
       } else {
-           cost += 20 * 0.12;
+        cost += 20 * 0.12;
 
-        // Calculate charges for next 10 cubic meters
-        if (consumed_water <= 60) {
-            cost += (consumed_water - 50) * 1.4;
+        if (consumption <= 60) {
+          cost += (consumption - 50) * 1.4;
         } else {
           cost += 10 * 1.4;
-
-          // Calculate charges for additional cubic meters
-          cost += (consumed_water - 60) * 1.5;
+          cost += (consumption - 60) * 1.5;
         }
       }
     }
   }
 
-
-    return cost;
+  // Display the total cost
+  cout << "Total cost: $" << cost << endl;
+return 0;
 }
+
+  
+ 
