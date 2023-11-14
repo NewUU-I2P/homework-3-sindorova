ac7c83e3f790dea68d53a7b632ac87b7727641e8
sindorova <147584619+sindorova@users.noreply.github.com>
2023-11-14 14:16:16 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,5 +2,34 @@ void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 
+     totalCost = 13.0;
+
+  // Calculate charges for first 30 cubic meters
+  if (consumed_water > 0) {
+    if (consumed_water <= 30) {
+      cost += consumed_water * 0.4;
+    } else {
+          cost += 30 * 0.4;
+
+      // Calculate charges for next 20 cubic meters
+      if (consumed_water <= 50) {
+           cost += (consumed_water - 30) * 0.12;
+      } else {
+           cost += 20 * 0.12;
+
+        // Calculate charges for next 10 cubic meters
+        if (consumed_water <= 60) {
+            cost += (consumed_water - 50) * 1.4;
+        } else {
+          cost += 10 * 1.4;
+
+          // Calculate charges for additional cubic meters
+          cost += (consumed_water - 60) * 1.5;
+        }
+      }
+    }
+  }
+
+
     return cost;
 }
