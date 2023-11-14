6ed6978c9cc404428407f53dd94e5c08cacf2e92
sindorova <147584619+sindorova@users.noreply.github.com>
2023-11-14 20:27:38 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,26 +1,28 @@
 #include <string>
 
-std::string problemSolution3(float height, char S) {
-    // write your code here
-    std::string result;
-    if(S == 'M') {
-        if(height < 1.70) {
-            result = "Short";
-        } else if(height > 1.70 and height < 1.85) {
-            result = "Normal";
-        } else if(height > 1.85) {
-            result = "Tall";
-        }
-    } else if(S == 'F') {
-        if(height < 1.60) {
-            result = "Short";
-        } else if(height > 1.60 and height < 1.75) {
-            result = "Normal";
-        } else {
-            result = "Tall";
-        }
+std::string problemSolution3(int height, std::string S) {
+  // write your code here
+  std::string result;
+  if (S == "M") {
+    if (height < 170) {
+      result = "Short";
+    } else if (height >= 170 && height < 185) {
+      result = "Normal";
+    } else if (height >= 185) {
+      result = "Tall";
     }
-    // use return to return your result
-    return result;
-    // make use of control flow statements
+  } else if (S == "F") {
+    if (height < 160) {
+      result = "Short";
+    } else if (height >= 160 && height < 175) {
+      result = "Normal";
+    } else {
+      result = "Tall";
+    }
+  } else {
+    result = "Invalid gender";
+  }
+  // use return to return your result
+  return result;
+  // make use of control flow statements
 }
